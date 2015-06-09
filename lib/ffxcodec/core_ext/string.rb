421577065d4f8c505d813f28c4d8cac3763ce80c
J. Brandt Buckley <brandt@runlevel1.com>
2015-06-08 21:20:13 -0700

    Add missing space

--- lib/ffxcodec/core_ext/string.rb
+++ lib/ffxcodec/core_ext/string.rb
@@ -6,7 +6,7 @@ class String
     longest = [b1.length, b2.length].max
     b1 = [0] * (longest - b1.length) + b1
     b2 = [0] * (longest - b2.length) + b2
-    b1.zip(b2).map{ |a, b| a ^ b }.pack("C*")
+    b1.zip(b2).map { |a, b| a ^ b }.pack("C*")
   end
 
   def bisect
